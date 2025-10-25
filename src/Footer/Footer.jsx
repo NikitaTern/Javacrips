289096b9c472b777b50ffb75ae2f41cb15b30a22
Ternovenko <nikitaternovenko38@gamil.com>
2025-10-25 23:46:16 +0300

    Final Front

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
             <div className="footer-content">
                 <div className="footer-section">
                     <h3>Контакты</h3>
-                    <p>support@smartmaps.ru</p>
-                    <p>+7 (999) 123-45-67</p>
+                    <p>romandeminov@gmail.com</p>
+                    <p>+7 (928) 777-88-14</p>
                 </div>
                 <div className="footer-section">
                     <h3>О сервисе</h3>
@@ -17,7 +17,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer-bottom">
-                <p>&copy; 2024 SmartMaps. Все права защищены.</p>
+                <p>&copy; 2025 JavaCrips. Все права защищены.</p>
             </div>
         </footer>
     );
